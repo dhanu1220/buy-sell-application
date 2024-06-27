be930c5fc37664212f26cc7caa266b3a6a246f44
dhanu1220 <ndhanusri2003@gmail.com>
2024-06-27 20:12:10 +0530

    created response pages

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Product(models.Model):
+    def __str__(self):
+        return self.name #return name of product instead of product1 product2
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     desc = models.CharField(max_length=200)
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Product
 def index(request):
     return HttpResponse("hello")
 
 def products(request):
-    products = ["iphone","android","mac"]
-    return HttpResponse(products)
+    products = Product.objects.all()
+    context={
+        'products':products
+    }
+    return render(request,'myapp/index.html',context)
 
+def detail(request,id):
+    product = Product.objects.get(id=id)
+    context={
+        'product':product
+    }
+    return render(request,'myapp/detail.html',context)
