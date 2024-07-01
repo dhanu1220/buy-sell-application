dee358a7e6f86e45e495b7f763d4d1ce8cc56d06
dhanu1220 <ndhanusri2003@gmail.com>
2024-07-01 11:46:01 +0530

    register page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
+from .forms import NewUserForm
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('myapp/products')
+    form = NewUserForm()
+    context={
+        'form': form,
+    }
+    return render(request,'users/register.html',context)
+
