1ae102ce3f103c6631ecd8b71487cd34e156c154
dhanu1220 <ndhanusri2003@gmail.com>
2024-07-02 08:21:41 +0530

    created profile page

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    def __str__(self):
+        return self.user.username
+    image = models.ImageField(default='profile.jpg',upload_to='profile_pictures')
+    contact_number=models.CharField(max_length=100,default='9999')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
