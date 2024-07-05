0dc09114f41eacde2b3d0c346652325c4109af46
dhanu1220 <ndhanusri2003@gmail.com>
2024-07-05 12:49:46 +0530

    payment gateway

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from .models import Product, OrderDetail
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView,DetailView, TemplateView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy, reverse
 from django.http.response import HttpResponseNotFound, JsonResponse
@@ -114,32 +114,61 @@ def my_listings(request):
 
 @csrf_exempt
 def create_checkout_session(request, id):
-    product = get_object_or_404(Product,pk=id)
+    product = get_object_or_404(Product, pk=id)
     stripe.api_key = settings.STRIPE_SECRET_KEY
-    checkout_session = stripe.checkout.Session.create(
-        customer_email= request.user.email,
-        payment_method_types=['card'],
-        line_items=[
-            {
-                'price_data':{
-                    'currency':'usd',
-                    'product_data':{
-                        'name':product.name,
+    
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            customer_email=request.user.email,
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': product.name,
+                        },
+                        'unit_amount': int(product.price * 100),
                     },
-                    'unit_amount':int(product.price*100),
-                },
-                'quantity':1,
-            }
-        ],
-        mode ='payment',
-        success_url = request.build_absolute_uri(reverse('myapp:success'))+"?session_id={CHECKOUT_SESSION_ID}",
-        cancel_url = request.build_absolute_uri(reverse('myapp:failed')),
+                    'quantity': 1,
+                }
+            ],
+            mode='payment',
+            success_url=request.build_absolute_uri(reverse('myapp:success')) + "?session_id={CHECKOUT_SESSION_ID}",
+            cancel_url=request.build_absolute_uri(reverse('myapp:failed')),
+        )
 
-    )
-    order = OrderDetail()
-    order.customer_username = request.user.customer_username
-    order.product = product
-    order.stripe_payment_intent = checkout_session['payment_intent']
-    order.amount = int(product.price*100)
-    order.save()
-    return JsonResponse({'sessionId':checkout_session.id})
+        # Debug logging
+        print(checkout_session)
+
+        order = OrderDetail()
+        order.customer_username = request.user.username
+        order.product = product
+        order.stripe_payment_intent = checkout_session.get('id')  # Use 'id' instead of 'payment_intent'
+        order.amount = int(product.price * 100)
+        order.save()
+
+        return JsonResponse({'sessionId': checkout_session.id})
+
+    except Exception as e:
+        return JsonResponse({'error': str(e)}, status=500)
+
+class PaymentSuccessView(TemplateView):
+    template_name = 'myapp/payment_success.html'
+
+    def get(self, request, *args, **kwargs):
+        session_id = request.GET.get('session_id')
+        if session_id is None:
+            return HttpResponseNotFound()
+        
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        session = stripe.checkout.Session.retrieve(session_id)
+        
+        order = get_object_or_404(OrderDetail, stripe_payment_intent=session.id)  # Retrieve using session id
+        order.has_paid = True
+        order.save()
+
+        return render(request, self.template_name)
+
+class PaymentFailedView(TemplateView):
+    template_name = 'myapp/payment_fail.html'
